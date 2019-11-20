1c8dd5556f31c6053da8d5bceacda058666db9f3
cyan <snow22314@outlook.com>
2019-11-20 18:19:15 +0800

    common temp save

---
+++ Tool/Kit/Main/Rice/Chip/Common/common-chip-core/src/main/java/cyan/tool/kit/common/core/CommonChipCoreApplication.java
@@ -0,0 +1,13 @@
+package cyan.tool.kit.common.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonChipCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonChipCoreApplication.class, args);
+    }
+
+}
---
+++ Tool/Kit/Main/Rice/Chip/Common/common-chip-flux/src/main/java/cyan/tool/kit/common/flux/CommonChipFluxApplication.java
@@ -0,0 +1,9 @@
+package cyan.tool.kit.common.flux;
+
+public class CommonChipFluxApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
