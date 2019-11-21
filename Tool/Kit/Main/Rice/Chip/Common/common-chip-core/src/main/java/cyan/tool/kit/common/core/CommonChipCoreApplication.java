f417af3204a2a8306a254ebc6b561079685a6f1e
cyan <snow22314@outlook.com>
2019-11-21 08:58:26 +0800

    rebuild common catalog

--- Tool/Kit/Main/Rice/Chip/Common/common-chip-core/src/main/java/cyan/tool/kit/common/core/CommonChipCoreApplication.java
+++ Tool/Kit/Main/Rice/Chip/Common/common-chip-core/src/main/java/cyan/tool/kit/common/core/CommonChipCoreApplication.java
@@ -1,13 +1,11 @@
 package cyan.tool.kit.common.core;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
 public class CommonChipCoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CommonChipCoreApplication.class, args);
+
     }
 
 }
