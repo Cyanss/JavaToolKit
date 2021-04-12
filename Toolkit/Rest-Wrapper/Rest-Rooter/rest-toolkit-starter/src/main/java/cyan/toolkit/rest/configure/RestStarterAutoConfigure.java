84e644fb5611273a2e5599949ce7ee1f233f2be0
cyans <snow22314@outlook.com>
2021-04-12 16:50:55 +0800

    rest-toolkit deploy

--- Toolkit/Rest-Wrapper/Rest-Rooter/rest-toolkit-starter/src/main/java/cyan/toolkit/rest/configure/RestStarterAutoConfigure.java
+++ Toolkit/Rest-Wrapper/Rest-Rooter/rest-toolkit-starter/src/main/java/cyan/toolkit/rest/configure/RestStarterAutoConfigure.java
@@ -1,7 +1,6 @@
 package cyan.toolkit.rest.configure;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
