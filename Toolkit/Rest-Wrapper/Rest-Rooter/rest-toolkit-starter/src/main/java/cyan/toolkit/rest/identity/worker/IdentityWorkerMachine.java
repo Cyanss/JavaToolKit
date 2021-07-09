84f27475dfd8da9c806904429495c98b0907e829
cyans <snow22314@outlook.com>
2021-07-09 14:55:47 +0800

    id生成器优化

--- Toolkit/Rest-Wrapper/Rest-Rooter/rest-toolkit-starter/src/main/java/cyan/toolkit/rest/identity/worker/IdentityWorkerMachine.java
+++ Toolkit/Rest-Wrapper/Rest-Rooter/rest-toolkit-starter/src/main/java/cyan/toolkit/rest/identity/worker/IdentityWorkerMachine.java
@@ -65,7 +65,7 @@ class IdentityWorkerMachine implements IdentityWorker{
                 offset = IdentityWorkerConfig.CACHE_SIZE + 1;
                 isOffset = false;
             }
-            time = new IdentityWorkerTime().sequence(Math.abs(this.lastTime - time) * this.sequence + offset);
+            time = new IdentityWorkerTime().sequence(Math.abs(this.lastTime - time) + this.sequence + offset);
             log.warn("clock is moving backwards. Rejecting requests until {}", this.lastTime);
         }
         if (this.lastTime.equals(time)) {
